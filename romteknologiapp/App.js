e19cd0407e5ac1536049b1c488101e64aefd2fae
ingunnsund <ingunnsu@online.no>
2019-04-01 23:37:02 +0200

    Design improvements

--- romteknologiapp/App.js
+++ romteknologiapp/App.js
@@ -5,6 +5,8 @@ import {
   Text,
   View,
   TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
   NativeAppEventEmitter,
   NativeEventEmitter,
   NativeModules,
@@ -22,19 +24,24 @@ import {
   TextInput,
   ViewPagerAndroid,
   Picker,
+  Modal,
+  Alert,
+  Slider,
 } from 'react-native';
-import Slider from "react-native-slider";
-import { BleManager } from 'react-native-ble-plx';
+//import Slider from "react-native-slider";
+import { BleManager, Subscription } from 'react-native-ble-plx';
 import RadialGradient from 'react-native-radial-gradient';
 import PlanetView from './components/Planet';
 import Arrow from './components/Arrow';
-import { stringToBytes, bytesToString } from 'convert-string';
 import base64 from 'react-native-base64'
 
 const windowSize = Dimensions.get('window');
-
+const planetNumber = 10;
+const initialPlanet = 3;
+const transactionID = '0';
 
 /*
+Debug commands:
 Reload: adb shell input text "RR"
 Dev menu: adb shell input keyevent 82
 */
@@ -42,6 +49,8 @@ Dev menu: adb shell input keyevent 82
 
 // Sett timeout ved sending
 // Disable send knapp hvis bluetooth er av
+// spør om posisjon, deprecatred
+// arrow margin11
 
 export default class App extends Component {
 	constructor(){
@@ -50,18 +59,19 @@ export default class App extends Component {
 		this.manager = new BleManager();
 
 		this.state = {
-			info: "", 
+			info: '', 
 			values: {},
-			rightOpacity: 1,
-			leftOpacity: 1,
-			device: "",
+			rightOpacity: 0.5, //1
+			leftOpacity: 0.5, //1
+			device: '',
 			content: windowSize.width*4, 
 			text: '0.1',
 			bluetoothSymbol: '#990000',
 			page: 1,
-			sliderValue: 0.4,
+			sliderValue: initialPlanet,
 			buttonDisable: true,
-			connect: "Not connected",
+			connect: 'Not connected',
+			modalVisible: false,
 		}
 
 		this.handleArrowScroll = this.handleArrowScroll.bind(this);
@@ -72,7 +82,7 @@ export default class App extends Component {
 	}
 
 	serviceUUID() {
-		return this.prefixUUID + "0" + this.suffixUUID
+		return this.prefixUUID + '0' + this.suffixUUID
 	}
 
 	characteristicWUUID() {
@@ -182,13 +192,13 @@ export default class App extends Component {
 			}
 			this.updateValue(characteristic.uuid, base64.decode(characteristic.value))
 			console.log("Received: " + base64.decode(characteristic.value))
-		})
+		}, transactionID);
 	}
 
 	sendData(data) {
 		const service = this.serviceUUID();
 		const characteristic = this.characteristicWUUID();
-		
+
 		this.state.device.writeCharacteristicWithResponseForService(service, characteristic, base64.encode(data)).then(ch => {
 			console.log("Sent: " + base64.decode(ch.value));
 		}).catch(error => {
@@ -203,7 +213,7 @@ export default class App extends Component {
 	handleArrowScroll(event) {
 		if(event === 0) {
 			this.setState({leftOpacity: 0});
-		} else if(event === 10) {
+		} else if(event === planetNumber-1) {
 			this.setState({rightOpacity: 0});
 		} else {
 			this.setState({leftOpacity: 0.5});
@@ -213,33 +223,110 @@ export default class App extends Component {
 
 	handlePlanetScroll(event) {
 		this.handleArrowScroll(event);
-		this.setState({sliderValue: Number((event/10).toFixed(1))});
+		this.setState({sliderValue: event});
 	}
 
 	handleSliderScroll(value) {
-		newPage = Number((value).toFixed(1))*10;
-		this.refs.planetScroll.setPage(newPage);
-		this.handleArrowScroll(newPage);
+		this.refs.planetScroll.setPage(value);
+		this.handleArrowScroll(value);
+		this.setState({sliderValue: value});
 	}
-	// TODO: merkur -> sol, slide til merkur -> sol. 
+	
+	handleArrowPress(direction) {
+		if(direction === 'Right') {
+			this.handlePlanetScroll(this.state.sliderValue + 1);
+			this.handleSliderScroll(this.state.sliderValue + 1);
+		} else {
+			this.handlePlanetScroll(this.state.sliderValue - 1);
+			this.handleSliderScroll(this.state.sliderValue - 1);
+		}
+	}
+
+	setModalVisible(visible) {
+		this.setState({modalVisible: visible});
+	}	
 
 	render() {
 		let values = [];
 		let max = 4;
 		let min = 0;
 
+		
 		for(let i = min*10; i <= max*10; i++){
 			values.push(
 				<Picker.Item label={(i/10).toFixed(1).toString()} value={(i/10).toFixed(1).toString()} key={(i/10).toFixed(1).toString()} />
 			)
 		}
+		values.push(
+			<Picker.Item label={"-1"} value={"-1"} key={"4.1"} />
+		)
+		/**
+		 * <Text style={styles.text}>
+					Trykk på en planet eller velg verdi for å sette G
+				</Text>
+		 */
+
+		//https://www.flaticon.com/free-icon/information_906794#term=question%20mark&page=1&position=2
+		//https://www.flaticon.com/free-icon/cancel_126497#term=cross&page=1&position=3
 		return (
 			<View style={styles.container}>
 				<StatusBar
 					backgroundColor="#0b172b"
 					barStyle="light-content"
 				/>
-				<View style={styles.bluetoothContainer}>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={this.state.modalVisible}
+					onRequestClose={() => {
+						console.log('Modal has been closed.');
+					}}>
+					<TouchableOpacity 
+						style={styles.modalContainer} 
+						activeOpacity={1} 
+						onPressOut={() => {this.setModalVisible(false)}}>
+
+						<ScrollView 
+							directionalLockEnabled={true} 
+							contentContainerStyle={styles.scrollModal}>
+						
+							<View style={{
+								marginTop: windowSize.width*1/8,
+								flex: 1,
+								flexDirection: 'column',
+								justifyContent: 'center',
+								alignItems: 'center'}}>
+								<TouchableWithoutFeedback>
+								<View style={{
+									backgroundColor: '#000000',
+									width: windowSize.width*4/5,
+									height: windowSize.height*4/5,
+									borderRadius: 18,
+									elevation: 2,
+									opacity: 0.6,
+									shadowColor: 'black', shadowOffset: { width: 0, height: 8 }, shadowOpacity: 0.16, shadowRadius: 16}}>
+								
+									<TouchableHighlight
+										onPress={() => {
+										this.setModalVisible(!this.state.modalVisible);
+										}}>
+										<Image 
+											style={styles.cancel}
+											source={require('romteknologiapp/images/cancel.png')}
+											
+										/>
+									</TouchableHighlight>
+									<Text>Om prosjekt, om tyngdekraft, hvordan bruke appen osv.</Text> 
+								</View>
+								</TouchableWithoutFeedback>
+							</View>
+						
+						</ScrollView>
+					</TouchableOpacity>   
+					
+				</Modal>
+				
+				<View style={styles.topContainer}>
 					<Image 
 						style={[styles.bluetooth, {tintColor: this.state.bluetoothSymbol}]}
 						source={require('romteknologiapp/images/bluetooth.png')}
@@ -247,102 +334,125 @@ export default class App extends Component {
 					<Text style={[styles.text/*, {position: 'absolute'}*/]}>
 						{(this.state.connect)}
 					</Text>
+					<View style={{/*flexDirection: 'row', *//*justifyContent: 'flex-end'*/ position: 'absolute', right: windowSize.width/40}}> 
+					<TouchableOpacity
+						onPress={() => {
+							this.setModalVisible(!this.state.modalVisible);
+							this.manager.cancelTransaction(transactionID);
+							}}>
+						<Image 
+							style={styles.question}
+							source={require('romteknologiapp/images/question.png')}	
+						/>
+					</TouchableOpacity>
+					
+					</View>
+					
 				</View>
-				<Text style={styles.text}>
-					Trykk på en planet eller velg verdi for å sette G
-				</Text>
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>
+						Space Bungalow
+					</Text>
+				</View>	
 				<View style={styles.container1}>
 					<ViewPagerAndroid
 						ref="planetScroll"
 						style={styles.viewPager}
-						initialPage={4}
+						initialPage={3}
 						onPageSelected={(page) => {
 							this.handlePlanetScroll(page.nativeEvent.position)
 						}}>
-						<View key="1"> 
-							<PlanetView planet="sun" />
-						</View>
-						<View key="2">
+						<View key="1">
 							<PlanetView planet="mercury" />
 						</View>
-						<View key="3">
+						<View key="2">
 							<PlanetView planet="venus" />
 						</View>
-						<View key="4">
+						<View key="3">
 							<PlanetView planet="moon" />
 						</View>
-						<View key="5">
+						<View key="4">
 							<PlanetView planet="earth" />
 						</View>
-						<View key="6">
+						<View key="5">
 							<PlanetView planet="mars" />
 						</View>
-						<View key="7">
+						<View key="6">
 							<PlanetView planet="jupiter" />
 						</View>
-						<View key="8">
+						<View key="7">
 							<PlanetView planet="saturn" />
 						</View>
-						<View key="9">
+						<View key="8">
 							<PlanetView planet="uranus" />
 						</View>
-						<View key="10">
+						<View key="9">
 							<PlanetView planet="neptune" />
 						</View>
-						<View key="11">
-							<PlanetView planet="pluto" />
+						<View key="10">
+							<PlanetView planet="spaceStation" />
 						</View>
 					</ViewPagerAndroid>
 
 					<Arrow>
-					<View style={[styles.arrowLeft, {opacity: this.state.leftOpacity}]}>
-						<Image 
-							style={styles.arrow1}
-							source={require('romteknologiapp/images/left-arrow.png')}
-							/>
-					</View>
-					<View style={[styles.arrowRight, {opacity: this.state.rightOpacity}]}>
-						<Image 
-							style={styles.arrow1}
-							source={require('romteknologiapp/images/right-arrow.png')}
-							/>
-					</View>
+						<View style={[styles.arrowLeft, {opacity: this.state.leftOpacity}]}>
+							{this.state.leftOpacity !== 0 && 
+							<TouchableOpacity
+								onPress={() => {
+									this.handleArrowPress("Left");
+								}}>
+								<Image 
+									style={styles.arrow1}
+									source={require('romteknologiapp/images/left-arrow.png')}
+								/>
+							</TouchableOpacity>
+							}
+						</View>
+						
+						<View style={[styles.arrowRight, {opacity: this.state.rightOpacity}]}>
+							{this.state.rightOpacity !== 0 && 
+							<TouchableOpacity
+								onPress={() => {
+									this.handleArrowPress("Right");
+								}}>
+								<Image 
+									style={styles.arrow1}
+									source={require('romteknologiapp/images/right-arrow.png')}
+								/>
+							</TouchableOpacity>
+							}
+						</View>
 					</Arrow >		
 				</View>
 				<View>
 					<Slider 
-						step={1/10}
+						step={1}
+						maximumValue={planetNumber-1}
 						//thumbTintColor='blue'
 						//style={{thumbTintColor='blue'}}
 						thumbTouchSize={{width: 60, height: 60}}
-						thumbImage={require('romteknologiapp/images/earth.png')}
-						thumbStyle={styles.thumb}
+						//thumbImage={require('romteknologiapp/images/earth.png')}
+						//thumbStyle={styles.thumb}
 						ref="slider"
 						value={this.state.sliderValue}
-						minimumTrackTintColor='#bababa'
-						maximumTrackTintColor='#bababa'
+						minimumTrackTintColor='transparent' //'#bababa'
+						maximumTrackTintColor='white'
+						//maximumTrackTintColor='transparent'  
+                		//minimumTrackTintColor='transparent'
 						style={styles.slider}
 						thumbTintColor='#FFFFFF'
 						onValueChange={(value) =>
 							this.handleSliderScroll(value)}>
 					</Slider>
-					<Text style={styles.text}>{this.state.info}</Text>
-					<Text style={styles.text}>
-						{(this.state.values[this.characteristicNUUID()] || "Test")}
-					</Text>
-					<Picker
-						selectedValue={this.state.language}
-						style={{height: 50, width: 100, backgroundColor: '#FFFFFF'}}
-						onValueChange={(itemValue, itemIndex) => {
-							this.setState({language: itemValue});
-							this.setState({text: itemValue});
-						}}>
-						{ values }
-					</Picker>
-					<Button 
-						title="Send"
-						disabled={this.state.buttonDisable}
-					 	onPress={() =>  this.sendData((this.state.text))} />
+					<View style={{/*flexDirection: 'row',*/ alignItems: 'center', justifyContent: 'center', borderColor: 'black', borderRadius: 4,
+    borderWidth: 0.5,}}>
+						<Text style={[styles.text, {fontSize: 39}]}>Gravitasjonskraft på romstasjonen: </Text>
+						<Text style={[styles.text, {fontSize: 39}]}>
+							{(this.state.values[this.characteristicNUUID()] || "0") + " g"}
+						</Text>
+					</View>
+					
+					
 				</View>
 				
 			</View>
@@ -363,21 +473,38 @@ const styles = StyleSheet.create({
 		height: windowSize.width * 8/10,
 		//backgroundColor: '#F5FCFF',
 	},
-	bluetoothContainer: {
+	modalContainer: {
+		flex: 1,
+		/*backgroundColor: '#122544',*/
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	topContainer: {
 		flexDirection: 'row',	
 		alignItems: 'center',
+		marginTop: windowSize.height * 1/150
 	},
 	bluetooth: {
 		width: 30,
 		height: 30,
 	},
+	question: {
+		width: 30,
+		height: 30,
+		tintColor: '#FFFFFF',
+	},
+	cancel: {
+		width: 30,
+		height: 30,
+		
+	},
 	arrowLeft: {
 		position: 'absolute',
-		right: windowSize.width * 2/5,
+		right: windowSize.width * 31/80,
 	},
 	arrowRight: {
 		position: 'absolute',
-		left: windowSize.width * 2/5,
+		left: windowSize.width * 31/80,//2/5,
 	},
 	arrow1: {
 		width: windowSize.width * 2/20,
@@ -390,6 +517,16 @@ const styles = StyleSheet.create({
 		color: '#FFFFFF',
 		fontFamily: 'OpenSans-Regular',
 	},
+	title: {
+		color: '#FFFFFF',
+		fontFamily: 'Orbitron-Regular',
+		fontSize: windowSize.width/14,
+		marginBottom: windowSize.height/60,
+	},
+	titleContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 	thumb: {
 		width: 60,
 		height: 60,
@@ -400,6 +537,7 @@ const styles = StyleSheet.create({
 	  },
 	slider: {
 		//color: '#6addaf'
+		height: 30
 	},
 	input: {
 		backgroundColor: '#FFFFFF'
--- romteknologiapp/components/Planet.js
+++ romteknologiapp/components/Planet.js
@@ -11,51 +11,67 @@ import RadialGradient from 'react-native-radial-gradient';
 
 const windowSize = Dimensions.get('window');
 
-const planets = {
+const planets = { // https://pngtree.com/freepng/planet-planet_1330808.html
     sun: {
         name: 'Solen',
         url: require('romteknologiapp/images/sun.png'),
+        text: 'På Solen er det 28 g',
     }, 
     mercury: {
         name: 'Merkur',
         url: require('romteknologiapp/images/mercury.png'),
+        text: 'På Merkur er det 0.38 g',
     }, 
     venus: {
         name: 'Venus',
         url: require('romteknologiapp/images/venus.png'),
+        text: 'På Venus er det 0.9 g',
     },
     moon: {
         name: 'Månen',
         url: require('romteknologiapp/images/moon.png'),
+        text: 'På Månen er det 0.17 g',
     },
     earth: {
         name: 'Jorden',
         url: require('romteknologiapp/images/earth.png'),
+        text: 'På Jorden er det 1 g',
     },
     mars: {
         name: 'Mars', 
         url: require('romteknologiapp/images/mars.png'),
+        text: 'På Mars er det 0.38 g',
     },
     jupiter: {
         name: 'Jupiter',
         url: require('romteknologiapp/images/jupiter.png'),
+        text: 'På Jupiter er det 2.53 g',
     },
     saturn: {
         name: 'Saturn',
         url: require('romteknologiapp/images/saturn.png'),
+        text: 'På Saturn er det 1.07 g',
     },
     uranus: {
         name: 'Uranus',
         url: require('romteknologiapp/images/uranus.png'),
+        text: 'På Uranus er det 0.89 g',
     },
     neptune: {
         name: 'Neptun',
         url: require('romteknologiapp/images/neptune.png'),
+        text: 'På Neptun er det 1.14 g',
     },
     pluto: {
         name: 'Pluto',
         url: require('romteknologiapp/images/pluto.png'),
+        text: 'På Pluto er det 0.06 g',
     },
+    spaceStation: {
+        name: 'Velg gravitasjon',
+        url: require('romteknologiapp/images/romstasjon.png'),
+        text: '',
+    }
 };
 
 
@@ -67,7 +83,7 @@ export default class PlanetView extends Component {
 	}
 
 	render() {
-		const {name, url} = planets[this.props.planet];
+		const {name, url, text} = planets[this.props.planet];
 		let planetStyle = styles.planets;
 		if(this.props.planet === 'sun') {
 			planetStyle = styles.planetSun;
@@ -95,7 +111,7 @@ export default class PlanetView extends Component {
                     </View>
 				
                 </RadialGradient>
-                <Text style={styles.infoText}>Planet text</Text>
+                <Text style={styles.infoText}>{text}</Text>
 			</View>												
 		);
 	}
@@ -112,14 +128,15 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		fontSize: windowSize.width * 1/20,
 		color: '#FFFFFF',
-        marginBottom: 5,
+        marginBottom: windowSize.width * 1/40,
         fontFamily: 'Orbitron-Regular',
     },
     infoText: {
 		textAlign: 'center',
-		fontSize: windowSize.width * 1/40,
+		fontSize: windowSize.width * 1/30, 
 		color: '#FFFFFF',
         marginBottom: 5,
+        marginTop: windowSize.width * 1/40,
         fontFamily: 'OpenSans-Regular',
 	},
 	planets: {
